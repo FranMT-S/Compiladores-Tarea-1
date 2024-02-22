1206c7419759a7100fa55e6c7b8b665126bbbce1
ArlethC <130802349+ArlethC@users.noreply.github.com>
2024-02-21 20:28:09 -0600

    DecimalConverter

--- Ejercicio 2/src/core/DecimalConverter.py
+++ Ejercicio 2/src/core/DecimalConverter.py
@@ -5,16 +5,54 @@ import re
 class DecimalConverter(IConverter):
    
     def ToBinary(self,number) -> str:
-        return "decimal a Binario"
+        if(not self.IsValid(number)):
+            return "No es un numero decimal valido"
+        
+        binaryNumber = " "
+        number = int(number)
+
+        while (number > 0):
+            binaryNumber += str(number%2)
+            number //= 2
+        binaryNumber = "".join(reversed(binaryNumber))
+
+        return binaryNumber
 
     def ToOctal(self,number) -> str:
-       return "decimal a octal"
+        if(not self.IsValid(number)):
+            return "No es un numero decimal valido"
+        
+        octalNumber = " "
+        number = int(number)
+
+        while(number > 0):
+            octalNumber += str(number%8)
+            number //= 8
+        octalNumber = "".join(reversed(octalNumber))
+
+        return octalNumber
 
     def ToDecimal(self,number) -> str:
+        if (not self.IsValid(number)):
+            return "No es un numero decimal valido"
         return number
 
     def ToHexadecimal(self,number) -> str:
-        return "decimal a hexadecimal"
+        if(not self.IsValid(number)):
+            return "No es un numero decimal valido"
+        
+        numbersHex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+        hexNumber = ""
+        number = int(number)
+
+        while(number > 0):
+            hexNumber += numbersHex[number%16]
+            number //= 16
+        hexNumber = "".join(reversed(hexNumber))
+
+        return hexNumber
     
     def IsValid(self,number) -> bool:
-        pass
+        pattern = r'^[0-9]+(\.[0-9]+)?$'
+        result = re.match(pattern, number)
+        return bool(result)
